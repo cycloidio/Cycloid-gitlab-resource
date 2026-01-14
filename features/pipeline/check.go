26004569c6a7176c7cac64dcd63de07e9ccc2385
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2026-01-14 12:05:42 +0100

    check: fix version output on check

--- features/pipeline/check.go
+++ features/pipeline/check.go
@@ -3,28 +3,19 @@ package pipeline
 import (
 	"fmt"
 
-	gitlabclient "github.com/cycloidio/gitlab-resource/clients/gitlab"
 	"github.com/cycloidio/gitlab-resource/internal"
 	"github.com/cycloidio/gitlab-resource/models"
 	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
 
 func (h *Handler) Check() error {
-	glab, err := gitlabclient.NewGitlabClient(&gitlabclient.GitlabConfig{
-		Token: h.cfg.Source.Token,
-		Url:   h.cfg.Source.ServerURL,
-	})
-	if err != nil {
-		return err
-	}
-
 	if h.cfg.Source.Pipeline.CheckMode == nil {
 		h.cfg.Source.Pipeline.CheckMode = gitlab.Ptr(models.Get)
 	}
 
 	switch *h.cfg.Source.Pipeline.CheckMode {
 	case models.Get:
-		pipeline, _, err := glab.Pipelines.GetLatestPipeline(h.cfg.Source.ProjectID,
+		pipeline, _, err := h.glab.Pipelines.GetLatestPipeline(h.cfg.Source.ProjectID,
 			&gitlab.GetLatestPipelineOptions{Ref: h.cfg.Source.Pipeline.Ref},
 		)
 		if err != nil {
@@ -35,7 +26,7 @@ func (h *Handler) Check() error {
 			)
 		}
 
-		return internal.PrintJSON(h.stdout, PipelinetoVersion(pipeline))
+		return internal.PrintJSON(h.stdout, []map[string]string{PipelinetoVersion(pipeline)})
 	default:
 		return fmt.Errorf("unsuported check mode %q", *h.cfg.Source.Pipeline.CheckMode)
 	}
